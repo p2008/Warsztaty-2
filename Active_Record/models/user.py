2a7ca2509045118d8297a775a3227a03f610c549
Czarek Baranski <c.baranski@protonmail.com>
2018-09-09 15:24:51 +0200

    user.py

---
+++ Active_Record/models/user.py
@@ -0,0 +1,26 @@
+
+
+class User(object):
+    __id = None
+    username = None
+    __hashed_password = None
+    email = None
+
+    def __init__(self):
+        self.__id = -1
+
+        self.username = ""
+        self.email = ""
+        self.__hashed_password = ""
+
+    @property
+    def id(self):
+        return self.__id
+
+    @property
+    def hashed_password(self):
+        return self.__hashed_password
+
+    #password_hash missing
+    def set_password(self, password, salt):
+        self.__hashed_password = password_hash(password, salt)
